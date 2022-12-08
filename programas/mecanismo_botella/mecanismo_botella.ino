bdfca1760407582808cf6dbf854eeabbcb3a3ed5
csanrod <c.sanchezro.2018@alumnos.urjc.es>
2022-12-08 14:07:50 +0100

    código + mejora actualizados, queda limpiar variables y seguir imlementando mejoras

--- programas/mecanismo_botella/mecanismo_botella.ino
+++ programas/mecanismo_botella/mecanismo_botella.ino
@@ -28,6 +28,10 @@ unsigned long move_start_time;
 bool glass_home,
      bottle_home;
 
+const int redLed = 3,
+          yellowLed = 4,
+          greenLed = 6;
+
 void setup() {
   Serial.begin(9600);
   current_state = WAITING_BUTTON;
@@ -49,87 +53,53 @@ void setup() {
   // Button
   button_value = LOW;
   pinMode(PIN_BUTTON, INPUT);
+
+  // Leds
+  pinMode(redLed , OUTPUT);
+  pinMode(yellowLed , OUTPUT);
+  pinMode(greenLed , OUTPUT);
+
+  digitalWrite(redLed , LOW);
+  digitalWrite(yellowLed , LOW);
+  digitalWrite(greenLed , LOW);
 }
 
 void loop() {
-  button_value = digitalRead(PIN_BUTTON); // pull up?? bouncing??
   
-  // check_button(button_value, current_state, move_start_time)  
-  if (button_value == HIGH) && (current_state == WAITING_BUTTON){
-    current_state = INIT_TO_POURING_G;
-    move_start_time = millis();
-  }
-    
+  button_value = digitalRead(PIN_BUTTON);
+  check_button();    
   
   if (current_state != WAITING_BUTTON) {
     unsigned long progress = millis() - move_start_time;
     
     switch (current_state){
       case INIT_TO_POURING_G:
-        // move_glass_to_pouring_pose();
-        if (progress <= GLASS_MOVING_TIME) {
-          long angle = map(progress, 0, GLASS_MOVING_TIME, glass_init_angle, glass_end_angle);
-          servo_glass.write(angle);
-        } else {
-          current_state = INIT_TO_POURING_B;
-          move_start_time = millis();
-          glass_home = false;
-        }
-        
+        move_glass_to_pouring_pose(progress);
         break;
         
       case INIT_TO_POURING_B:
-        // move_bottle_to_pouring_pose();
-        if (progress <= BOTTLE_MOVING_TIME) {
-          long angle = map(progress, 0, BOTTLE_MOVING_TIME, bottle_init_angle, bottle_end_angle);
-          servo_bottle.write(angle); 
-        } else {
-          current_state = POURING;
-          move_start_time = millis();
-          bottle_home = false;
-        }
-        
+        move_bottle_to_pouring_pose(progress);
         break;
         
       case POURING:
-        // pouring();
-        if (progress >= POURING_TIME) {
-          current_state = GOING_HOME;
-          move_start_time = millis();
-        }
-      
+        pouring(progress);
         break;
         
       case GOING_HOME:
-        // set_all_to_default();
-        if (progress <= GLASS_MOVING_TIME) {
-          long angle = map(progress, 0, GLASS_MOVING_TIME, glass_end_angle, glass_init_angle);
-          servo_glass.write(angle);
-        } else 
-          glass_home = true;
-
-        if (progress <= BOTTLE_MOVING_TIME) {
-          long angle = map(progress, 0, BOTTLE_MOVING_TIME, bottle_end_angle, bottle_init_angle);
-          servo_bottle.write(angle); 
-        } else
-          bottle_home = true;
-
-        if (glass_home && bottle_home)
-          current_state = WAITING_BUTTON;
-        
+        set_all_to_default(progress);        
         break;      
     }
   }
 }
 
 void check_button() {
-  if (button_value == HIGH) && (current_state == WAITING_BUTTON){
+  if ((button_value == HIGH) && (current_state == WAITING_BUTTON)){
     current_state = INIT_TO_POURING_G;
     move_start_time = millis();
   }
 }
 
-void move_glass_to_pouring_pose(){
+void move_glass_to_pouring_pose(unsigned long progress){
   if (progress <= GLASS_MOVING_TIME) {
     long angle = map(progress, 0, GLASS_MOVING_TIME, glass_init_angle, glass_end_angle);
     servo_glass.write(angle);
@@ -140,7 +110,7 @@ void move_glass_to_pouring_pose(){
   }
 }
 
-void move_bottle_to_pouring_pose() {
+void move_bottle_to_pouring_pose(unsigned long progress) {
   if (progress <= BOTTLE_MOVING_TIME) {
     long angle = map(progress, 0, BOTTLE_MOVING_TIME, bottle_init_angle, bottle_end_angle);
     servo_bottle.write(angle); 
@@ -151,14 +121,25 @@ void move_bottle_to_pouring_pose() {
   }
 }
 
-void pouring() {
+void pouring(unsigned long progress) {
   if (progress >= POURING_TIME) {
     current_state = GOING_HOME;
     move_start_time = millis();
   }
+
+  if (progress < (POURING_TIME/3)){
+    digitalWrite(redLed, HIGH);
+  } else if (progress < (POURING_TIME*(2/3))) {
+    digitalWrite(yellowLed , HIGH);
+  } else {
+    digitalWrite(greenLed , HIGH);
+  }
 }
 
-void set_all_to_default() {
+void set_all_to_default(unsigned long progress) {
+  digitalWrite(redLed , LOW);
+  digitalWrite(yellowLed , LOW);
+  digitalWrite(greenLed , LOW);
   if (progress <= GLASS_MOVING_TIME) {
     long angle = map(progress, 0, GLASS_MOVING_TIME, glass_end_angle, glass_init_angle);
     servo_glass.write(angle);
